e4812cb329887c6a7a6acb9e90255f8e88d51e94
Jack Tsai <jacktsai@dyn-209-2-212-243.dyn.columbia.edu>
2019-07-31 19:35:19 -0400

    first funtional test

--- tdd/lists/tests.py
+++ tdd/lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from lists.views import home_page
 
 
@@ -11,3 +12,10 @@ class HomePageTest(TestCase):
     def test_root_url(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
